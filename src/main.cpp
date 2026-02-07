200b3f8bf7865824537d459a080b760988902da2
Elektrospy <15101742+Elektrospy@users.noreply.github.com>
2026-02-08 00:31:29 +0100

    Added auto ui scale based on window size Added settings menu for ui scaling

--- src/main.cpp
+++ src/main.cpp
@@ -50,9 +50,11 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    const int initial_width = 1280;
+    const int initial_height = 720;
     // Create window with SDL3 flags
     Uint32 window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY;
-    SDL_Window *window = SDL_CreateWindow("InputBridge", 1280, 720, window_flags);
+    SDL_Window *window = SDL_CreateWindow("InputBridge", initial_width, initial_height, window_flags);
     if (!window) {
         printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
         return -1;
@@ -90,20 +92,6 @@ int main(int argc, char *argv[]) {
     ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
     ImGui_ImplSDLRenderer3_Init(renderer);
 
-    auto UpdateUIScale = [](SDL_Window *win) {
-        float scale = SDL_GetWindowDisplayScale(win);
-        float density = SDL_GetWindowPixelDensity(win);
-        if (density <= 0.0f) density = 1.0f;
-        if (scale <= 0.0f) scale = 1.0f;
-        float ui_scale = scale / density;
-
-        ImGuiStyle &style = ImGui::GetStyle();
-        ImGui::StyleColorsDark();
-        style.ScaleAllSizes(ui_scale);
-        ImGui::GetIO().FontGlobalScale = ui_scale;
-    };
-    UpdateUIScale(window);
-
     DeviceManager& deviceManager = DeviceManager::GetInstance();
     PreferencesManager preferencesManager;
 
@@ -119,6 +107,33 @@ int main(int argc, char *argv[]) {
 
     preferencesManager.Load();
 
+    float user_ui_scale = preferencesManager.GetFloat("UIScale", 1.0f);
+    bool scale_with_window = preferencesManager.GetBool("ScaleWithWindow", false);
+
+    auto UpdateUIScale = [&](SDL_Window *win) {
+        float scale = SDL_GetWindowDisplayScale(win);
+        float density = SDL_GetWindowPixelDensity(win);
+        if (density <= 0.0f) density = 1.0f;
+        if (scale <= 0.0f) scale = 1.0f;
+        float ui_scale = scale / density;
+
+        if (scale_with_window) {
+            int w, h;
+            SDL_GetWindowSize(win, &w, &h);
+            user_ui_scale = (float)w / (float)initial_width;
+            preferencesManager.SetFloat("UIScale", user_ui_scale);
+        }
+        ui_scale *= user_ui_scale;
+
+        ImGuiStyle &style = ImGui::GetStyle();
+        style = ImGuiStyle(); // Reset to default style to avoid compounding scales
+        ImGui::StyleColorsDark();
+        style.ScaleAllSizes(ui_scale);
+        if (style.WindowBorderHoverPadding <= 0.0f) style.WindowBorderHoverPadding = 1.0f;
+        ImGui::GetIO().FontGlobalScale = ui_scale;
+    };
+    UpdateUIScale(window);
+
     InputMapper inputMapper(deviceManager);
     inputMapper.LoadConfig(preferencesManager);
 
@@ -129,6 +144,7 @@ int main(int argc, char *argv[]) {
     bool done = false;
     bool vsync = true;
     int framerate_limit = 60;
+    static bool show_ui_settings = false;
     SDL_SetRenderVSync(renderer, 1);
 
     while (!done) {
@@ -143,6 +159,10 @@ int main(int argc, char *argv[]) {
                 done = true;
 
             if (event.type == SDL_EVENT_WINDOW_DISPLAY_SCALE_CHANGED &&
+                event.window.windowID == SDL_GetWindowID(window)) {
+                UpdateUIScale(window);
+            }
+            if (event.type == SDL_EVENT_WINDOW_RESIZED && scale_with_window &&
                 event.window.windowID == SDL_GetWindowID(window)) {
                 UpdateUIScale(window);
             }
@@ -179,7 +199,44 @@ int main(int argc, char *argv[]) {
                 }
                 ImGui::EndMenu();
             }
+            if (ImGui::BeginMenu("Settings")) {
+                ImGui::MenuItem("UI Settings", NULL, &show_ui_settings);
+                ImGui::EndMenu();
+            }
             ImGui::EndMainMenuBar();
+        }
+
+        if (show_ui_settings) {
+            ImGui::Begin("UI Settings", &show_ui_settings);
+            bool changed = false;
+            bool scale_changed = false;
+            if (ImGui::Button("-")) {
+                user_ui_scale -= 0.05f;
+                if (user_ui_scale < 0.5f) user_ui_scale = 0.5f;
+                scale_changed = true;
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("+")) {
+                user_ui_scale += 0.05f;
+                if (user_ui_scale > 3.0f) user_ui_scale = 3.0f;
+                scale_changed = true;
+            }
+            ImGui::SameLine();
+            if (ImGui::SliderFloat("UI Scale", &user_ui_scale, 0.5f, 3.0f, "%.2f")) scale_changed = true;
+
+            if (scale_changed) {
+                scale_with_window = false;
+                changed = true;
+            }
+            if (ImGui::Checkbox("Scale with Window", &scale_with_window)) changed = true;
+            
+            if (changed) {
+                preferencesManager.SetFloat("UIScale", user_ui_scale);
+                preferencesManager.SetBool("ScaleWithWindow", scale_with_window);
+                preferencesManager.Save();
+                UpdateUIScale(window);
+            }
+            ImGui::End();
         }
 
         // DockSpace
