a6c76602ebc56c49cdb4059c6f98bdbfaad3a663
Elektrospy <15101742+Elektrospy@users.noreply.github.com>
2026-02-09 11:38:45 +0100

    Extended Dualsense trigger library with generic events

--- src/Haptics/DualSenseTriggerEffectGenerator.cpp
+++ src/Haptics/DualSenseTriggerEffectGenerator.cpp
@@ -1,36 +1,26 @@
 #include "DualSenseTriggerEffectGenerator.h"
 #include <cstring>
+#include <algorithm>
 
 void DualSenseTriggerEffectGenerator::ApplyToBuffer(uint8_t* buffer, std::size_t offset, const DualSenseTriggerEffect& effect) {
     buffer[offset] = effect.Mode;
-    std::memcpy(buffer + offset + 1, effect.Params, sizeof(effect.Params));
+    std::copy(effect.Params.begin(), effect.Params.end(), buffer + offset + 1);
 }
 
 DualSenseTriggerEffect DualSenseTriggerEffectGenerator::Off() {
-    DualSenseTriggerEffect effect = {0};
-    effect.Mode = 0x00;
-    return effect;
+    return CreateOff();
 }
 
 DualSenseTriggerEffect DualSenseTriggerEffectGenerator::Feedback(uint8_t startPosition, uint8_t strength) {
-    DualSenseTriggerEffect effect = {0};
-    effect.Mode = 0x01;
-    effect.Params[0] = startPosition;
-    effect.Params[1] = strength;
-    return effect;
+    return CreateFeedback(startPosition, strength);
 }
 
 DualSenseTriggerEffect DualSenseTriggerEffectGenerator::Weapon(uint8_t startPosition, uint8_t endPosition, uint8_t strength) {
-    DualSenseTriggerEffect effect = {0};
-    effect.Mode = 0x02;
-    effect.Params[0] = startPosition;
-    effect.Params[1] = endPosition;
-    effect.Params[2] = strength;
-    return effect;
+    return CreateWeapon(startPosition, endPosition, strength);
 }
 
 DualSenseTriggerEffect DualSenseTriggerEffectGenerator::Vibration(uint8_t position, uint8_t amplitude, uint8_t frequency) {
-    DualSenseTriggerEffect effect = {0};
+    DualSenseTriggerEffect effect{};
     effect.Mode = 0x26;
     effect.Params[0] = 0x01 | 0x02 | 0x04 | 0x08 | 0x10 | 0x20; // All regions
     effect.Params[1] = 0x00;
@@ -43,36 +33,91 @@ DualSenseTriggerEffect DualSenseTriggerEffectGenerator::Vibration(uint8_t positi
 }
 
 DualSenseTriggerEffect DualSenseTriggerEffectGenerator::Bow(uint8_t startPosition, uint8_t endPosition, uint8_t strength, uint8_t snapForce) {
-    DualSenseTriggerEffect effect = {0};
-    effect.Mode = 0x26;
-    effect.Params[0] = startPosition;
-    effect.Params[1] = endPosition;
-    effect.Params[2] = strength;
-    effect.Params[3] = snapForce;
-    effect.Params[4] = 0x00;
-    effect.Params[5] = 0x00;
-    effect.Params[6] = 0x00;
-    effect.Params[7] = 0x00;
-    return effect;
+    return CreateBow(startPosition, endPosition, strength, snapForce);
 }
 
 DualSenseTriggerEffect DualSenseTriggerEffectGenerator::Galloping(uint8_t startPosition, uint8_t endPosition, uint8_t firstFoot, uint8_t secondFoot, uint8_t frequency) {
-    DualSenseTriggerEffect effect = {0};
-    effect.Mode = 0x26;
-    effect.Params[0] = startPosition;
-    effect.Params[1] = endPosition;
-    effect.Params[2] = firstFoot;
-    effect.Params[3] = secondFoot;
-    effect.Params[4] = frequency;
-    return effect;
+    return CreateGalloping(startPosition, endPosition, firstFoot, secondFoot, frequency);
 }
 
 DualSenseTriggerEffect DualSenseTriggerEffectGenerator::MachineGun(uint8_t startPosition, uint8_t endPosition, uint8_t strength, uint8_t frequency) {
-    DualSenseTriggerEffect effect = {0};
-    effect.Mode = 0x26;
-    effect.Params[0] = startPosition;
-    effect.Params[1] = endPosition;
-    effect.Params[2] = strength;
-    effect.Params[3] = frequency;
-    return effect;
+    // The gist's CreateAutomatic takes period instead of frequency. Assuming they are meant to be the same for this mapping.
+    return CreateAutomatic(startPosition, endPosition, strength, frequency);
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateEffect(TriggerEffect effect, const std::vector<uint8_t> &parameters) {
+    return CreateEffect(static_cast<uint8_t>(effect), parameters);
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateEffect(uint8_t effect, const std::vector<uint8_t> &parameters) {
+    DualSenseTriggerEffect result{};
+    result.Mode = effect;
+    if (!parameters.empty()) {
+        std::copy_n(parameters.begin(), std::min(parameters.size(), result.Params.size()), result.Params.begin());
+    }
+    return result;
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateOff() {
+    return CreateEffect(TriggerEffect::Off, {});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateRigid(uint8_t start, uint8_t force) {
+    return CreateEffect(TriggerEffect::Rigid, {start, force});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateRigid_A(uint8_t start, uint8_t force) {
+    return CreateEffect(TriggerEffect::Rigid_A, {start, force, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateRigid_B(uint8_t start, uint8_t force) {
+    return CreateEffect(TriggerEffect::Rigid_B, {0x00, 0x00, start, force, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateRigid_AB(uint8_t start, uint8_t force) {
+    return CreateEffect(TriggerEffect::Rigid_AB, {start, force, start, force, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreatePulse(uint8_t start, uint8_t end, uint8_t force) {
+    return CreateEffect(TriggerEffect::Pulse, {start, end, force});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreatePulse_A(uint8_t start, uint8_t end, uint8_t force) {
+    return CreateEffect(TriggerEffect::Pulse_A, {start, end, force, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreatePulse_B(uint8_t start, uint8_t end, uint8_t force) {
+    return CreateEffect(TriggerEffect::Pulse_B, {0x00, 0x00, 0x00, start, end, force, 0x00, 0x00, 0x00, 0x00});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreatePulse_AB(uint8_t start, uint8_t end, uint8_t force) {
+    return CreateEffect(TriggerEffect::Pulse_AB, {start, end, force, start, end, force, 0x00, 0x00, 0x00, 0x00});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateFeedback(uint8_t position, uint8_t strength) {
+    return CreateEffect(TriggerEffect::Feedback, {position, strength});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateWeapon(uint8_t start, uint8_t end, uint8_t strength) {
+    return CreateEffect(TriggerEffect::Weapon, {start, end, strength});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateBow(uint8_t start, uint8_t end, uint8_t strength, uint8_t snapForce) {
+    return CreateEffect(TriggerEffect::Bow, {start, end, strength, snapForce});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateGalloping(uint8_t start, uint8_t end, uint8_t firstFoot, uint8_t secondFoot, uint8_t frequency) {
+    return CreateEffect(TriggerEffect::Galloping, {start, end, firstFoot, secondFoot, frequency});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateSemiAutomatic(uint8_t start, uint8_t end, uint8_t strength) {
+    return CreateEffect(TriggerEffect::SemiAutomatic, {start, end, strength});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateAutomatic(uint8_t start, uint8_t end, uint8_t strength, uint8_t period) {
+    return CreateEffect(TriggerEffect::Automatic, {start, end, strength, period});
+}
+
+DualSenseTriggerEffect DualSenseTriggerEffectGenerator::CreateMachine(uint8_t start, uint8_t strength, uint8_t frequency, uint8_t period) {
+    return CreateEffect(TriggerEffect::Machine, {start, 0x00, strength, frequency, 0x00, 0x00, period});
 }
--- src/Haptics/DualSenseTriggerEffectGenerator.h
+++ src/Haptics/DualSenseTriggerEffectGenerator.h
@@ -2,14 +2,40 @@
 
 #include <cstddef> // for std::size_t
 #include <cstdint> // for uint8_t
+#include <vector>
+#include <array>
 
 struct DualSenseTriggerEffect {
-    uint8_t Mode;
-    uint8_t Params[10];
+    uint8_t Mode{0};
+    std::array<uint8_t, 10> Params{};
 };
 
 class DualSenseTriggerEffectGenerator {
   public:
+    enum class TriggerEffect : uint8_t {
+        Off = 0x05,
+        // Simple
+        Rigid = 0x01,
+        Rigid_A = 0x01,
+        Rigid_B = 0x01,
+        Rigid_AB = 0x01,
+        // Pulse
+        Pulse = 0x02,
+        Pulse_A = 0x02,
+        Pulse_B = 0x02,
+        Pulse_AB = 0x02,
+        // Official
+        Feedback = 0x21,
+        Weapon = 0x25,
+        Bow = 0x22,
+        Galloping = 0x23,
+        SemiAutomatic = 0x24,
+        Automatic = 0x27,
+        Machine = 0x26,
+    };
+
+    DualSenseTriggerEffectGenerator() = delete;
+
     static DualSenseTriggerEffect Off();
     static DualSenseTriggerEffect Feedback(uint8_t startPosition, uint8_t strength);
     static DualSenseTriggerEffect Weapon(uint8_t startPosition, uint8_t endPosition, uint8_t strength);
@@ -20,4 +46,24 @@ class DualSenseTriggerEffectGenerator {
 
     // Helper to apply an effect to a specific trigger in the output report buffer
     static void ApplyToBuffer(uint8_t *buffer, std::size_t offset, const DualSenseTriggerEffect &effect);
+
+    static DualSenseTriggerEffect CreateEffect(TriggerEffect effect, const std::vector<uint8_t> &parameters);
+    static DualSenseTriggerEffect CreateEffect(uint8_t effect, const std::vector<uint8_t> &parameters);
+
+    static DualSenseTriggerEffect CreateOff();
+    static DualSenseTriggerEffect CreateRigid(uint8_t start, uint8_t force);
+    static DualSenseTriggerEffect CreateRigid_A(uint8_t start, uint8_t force);
+    static DualSenseTriggerEffect CreateRigid_B(uint8_t start, uint8_t force);
+    static DualSenseTriggerEffect CreateRigid_AB(uint8_t start, uint8_t force);
+    static DualSenseTriggerEffect CreatePulse(uint8_t start, uint8_t end, uint8_t force);
+    static DualSenseTriggerEffect CreatePulse_A(uint8_t start, uint8_t end, uint8_t force);
+    static DualSenseTriggerEffect CreatePulse_B(uint8_t start, uint8_t end, uint8_t force);
+    static DualSenseTriggerEffect CreatePulse_AB(uint8_t start, uint8_t end, uint8_t force);
+    static DualSenseTriggerEffect CreateFeedback(uint8_t position, uint8_t strength);
+    static DualSenseTriggerEffect CreateWeapon(uint8_t start, uint8_t end, uint8_t strength);
+    static DualSenseTriggerEffect CreateBow(uint8_t start, uint8_t end, uint8_t strength, uint8_t snapForce);
+    static DualSenseTriggerEffect CreateGalloping(uint8_t start, uint8_t end, uint8_t firstFoot, uint8_t secondFoot, uint8_t frequency);
+    static DualSenseTriggerEffect CreateSemiAutomatic(uint8_t start, uint8_t end, uint8_t strength);
+    static DualSenseTriggerEffect CreateAutomatic(uint8_t start, uint8_t end, uint8_t strength, uint8_t period);
+    static DualSenseTriggerEffect CreateMachine(uint8_t start, uint8_t strength, uint8_t frequency, uint8_t period);
 };
