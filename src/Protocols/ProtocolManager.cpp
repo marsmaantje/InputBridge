7f9b5c730cf12302b5a0be1e121b7d3f14ad3f86
Elektrospy <15101742+Elektrospy@users.noreply.github.com>
2026-03-03 23:30:39 +0100

    Added SteamLink OSC fields back to registry

--- src/Protocols/ProtocolManager.cpp
+++ src/Protocols/ProtocolManager.cpp
@@ -3,6 +3,7 @@
 #include "Protocols/MarsmaantjeOldProtocol.h"
 #include "Protocols/MarsmaantjeNewProtocol.h"
 #include "Protocols/OSCDefaultProtocol.h"
+#include "Protocols/OSCSteamLinkProtocol.h"
 
 struct ProtocolManager::Impl {
     std::map<std::string, std::shared_ptr<IProtocol>> protocols;
@@ -18,6 +19,7 @@ ProtocolManager::ProtocolManager() : m_Impl(new Impl) {
     RegisterProtocol(std::make_shared<MarsmaantjeOldProtocol>());
     RegisterProtocol(std::make_shared<MarsmaantjeNewProtocol>());
     RegisterProtocol(std::make_shared<OSCDefaultProtocol>());
+    RegisterProtocol(std::make_shared<OSCSteamLinkProtocol>());
 }
 
 ProtocolManager::~ProtocolManager() { delete m_Impl; }
